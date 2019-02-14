f1b47d12a145c25181b409f9ea12d2e26643111e
hgaros <hg@example.com>
2019-02-14 14:47:41 +0100

    testing the rest api

--- myrest.py
+++ myrest.py
@@ -5,18 +5,16 @@ import os
 
 app = Flask(__name__)
 
-tasks = [
+batch = [
 {
   'id': 1,
-  'title': u'Buy groceries',
-  'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-  'done': False
+  'name': u'Sensor1',
+  'values': u'1, 12, 27, 13, 111',
 },
 {
   'id': 2,
-  'title': u'Learn Python',
-  'description': u'Need to find a good Python tutorial on the web',
-  'done': False
+  'name': u'Sensor2',
+  'values': u'3, 5, 67, 14, 132'
 }
 ]
 
@@ -24,9 +22,9 @@ tasks = [
 def hello():
     return "Hello World!"
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
+@app.route('/todo/api/v1.0/sensors', methods=['GET'])
+def get_sensor_readings():
+    return jsonify({'batch': batch})
 
 @app.route('/todo/api/v1.0/battery_health', methods=['GET'])
 def get_battery_health():
