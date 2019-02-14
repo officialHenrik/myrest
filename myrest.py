c0384c4f2b82815054d110a4b1a615318ccddb3d
officialHenrik <36031881+officialHenrik@users.noreply.github.com>
2019-02-14 15:13:35 +0100

    Update myrest.py

--- myrest.py
+++ myrest.py
@@ -20,6 +20,8 @@ batch = [
 
 hello_word = 'world'
 
+# curl 127.0.0.1:5000/todo/api/v1.0/hello?word=fjant
+# curl 127.0.0.1:5000/todo/api/v1.0/hello
 @app.route("/todo/api/v1.0/hello", methods=['GET'])
 def hello():
     global hello_word
@@ -28,10 +30,12 @@ def hello():
         hello_word = tmp
     return "Hello " + hello_word + " !"
 
+# curl 127.0.0.1:5000/todo/api/v1.0/sensors
 @app.route('/todo/api/v1.0/sensors', methods=['GET'])
 def get_sensor_readings():
     return jsonify({'batch': batch})
-
+  
+# curl 127.0.0.1:5000/todo/api/v1.0/battery_health
 @app.route('/todo/api/v1.0/battery_health', methods=['GET'])
 def get_battery_health():
     p = os.popen('cat /sys/class/power_supply/battery/health', "r")
