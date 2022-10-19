d195573b64e655248bfd057044c77790ad8a3e96
Henrik Gullvén <henrik.gullven@evnxs.com>
2022-10-19 16:12:37 +0200

    add fake sine sensor

--- myrest.py
+++ myrest.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python3
 
 from flask import Flask, jsonify, request
-import os
+import os, threading, time
+import numpy as np
 
 app = Flask(__name__)
 
 batch = [
-{
-  'id': 1,
-  'name': u'Sensor1',
-  'values': u'1, 12, 27, 13, 111',
-},
-{
-  'id': 2,
-  'name': u'Sensor2',
-  'values': u'3, 5, 67, 14, 132'
-}
+    {
+    'id': 1,
+    'name': u'SensorX',
+    'value': 0
+    },
+    {
+    'id': 2,
+    'name': u'SensorSine',
+    'value': 0
+    }
 ]
 
 hello_word = 'world'
 
-# curl 127.0.0.1:5000/todo/api/v1.0/hello?word=fjant
-# curl 127.0.0.1:5000/todo/api/v1.0/hello
-@app.route("/todo/api/v1.0/hello", methods=['GET'])
+# curl 127.0.0.1:5000/api/v1.0/hello?word=fjant
+# curl 127.0.0.1:5000/api/v1.0/hello
+@app.route("/api/v1.0/hello", methods=['GET'])
 def hello():
     global hello_word
     tmp = request.args.get('word')
@@ -30,16 +31,32 @@ def hello():
         hello_word = tmp
     return "Hello " + hello_word + " !"
 
-# curl 127.0.0.1:5000/todo/api/v1.0/sensors
-@app.route('/todo/api/v1.0/sensors', methods=['GET'])
+# curl 127.0.0.1:5000/api/v1.0/sensors
+# curl 127.0.0.1:5000/api/v1.0/sensors?id=§
+@app.route('/api/v1.0/sensors', methods=['GET'])
 def get_sensor_readings():
-    return jsonify({'batch': batch})
-  
-# curl 127.0.0.1:5000/todo/api/v1.0/battery_health
-@app.route('/todo/api/v1.0/battery_health', methods=['GET'])
-def get_battery_health():
-    p = os.popen('cat /sys/class/power_supply/battery/health', "r")
-    return(p.readline())
+    
+    id = int(request.args.get('id'))
+    if not id:
+        return jsonify({'batch': batch})
+    idx = id-1
+    if len(batch) > idx:
+        return str(batch[idx]["value"])
+    else:
+        return "INVALID INDEX"
+
+# Fake sensor
+def sensor_sampler():
+    while 1:
+        batch[0]["value"]+=0.01
+        x = batch[0]["value"]
+        batch[1]["value"]=np.sin(x)*100
+        time.sleep(0.1)
 
 if __name__ == '__main__':
+    
+    x = threading.Thread(target=sensor_sampler)
+    x.daemon = True
+    x.start()
+    
     app.run(debug=True)
